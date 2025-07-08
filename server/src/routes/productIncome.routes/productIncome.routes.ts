b0cbc32750ea02fc5aa33f6ee55ba023ef933172
manuel2 <manuelrz098@gmail.com>
2025-07-07 21:34:45 -0500

    Iniciamos la parte del cliente

--- server/src/routes/productIncome.routes/productIncome.routes.ts
+++ server/src/routes/productIncome.routes/productIncome.routes.ts
@@ -7,11 +7,11 @@ const productIncomeRoute = Router();
 
 const { getSoldProducts, getSoldProduct, addSoldProduct, updateSoldProduct, deleteSoldProduct } = controllers
 
-productIncomeRoute.get( '/products-income', getSoldProducts );
-productIncomeRoute.get( '/products-income/:id', getSoldProduct );
-productIncomeRoute.post( '/products-income/:id', addSoldProduct );
-productIncomeRoute.put( '/products-income/:id', updateSoldProduct );
-productIncomeRoute.delete( '/products-income/:id', deleteSoldProduct );
+productIncomeRoute.get( '/products-sold', getSoldProducts );
+productIncomeRoute.get( '/products-sold/:id', getSoldProduct );
+productIncomeRoute.post( '/products-sold/:id', addSoldProduct );
+productIncomeRoute.put( '/products-sold/:id', updateSoldProduct );
+productIncomeRoute.delete( '/products-sold/:id', deleteSoldProduct );
 
 
 export {
